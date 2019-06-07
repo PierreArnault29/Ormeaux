fa291a397260eba02a6866b89b8883fc00fc39aa
PierreArnault29 <44776992+PierreArnault29@users.noreply.github.com>
2019-06-07 13:45:37 +0200

    Version V2.2-Ajout gestion batterie
    Ajout de la tension aux bornes de la batterie + pin déclenchement mesure de la trame SPI + explications sur le code(commentaires)

--- Programmes/Arduino/Fusions/Fusion-temp/Fusion-temp.ino
+++ Programmes/Arduino/Fusions/Fusion-temp/Fusion-temp.ino
@@ -1,3 +1,11 @@
+/*********************
+
+Example code for the Adafruit RGB Character LCD Shield and Library
+
+This code displays text on the shield, and also reads the buttons on the keypad.
+When a button is pressed, the backlight changes color.
+
+**********************/
 
 // include the library code:
 #include <OneWire.h>
@@ -7,22 +15,26 @@
 #include <EEPROM.h>
 #include <LoRa.h>
 #include <SPI.h>
-#include <RH_RF95.h>
+//#include <RH_RF95.h>
 
 #define RFM95_CS 40     //chip select
 #define RFM95_RST 41    // pins Lora
 #define RFM95_INT 19    // interrupt
 
-#define RF95_FREQ 915.0
+#define RF95_FREQ 868.2E6
 
 // Singleton instance of the radio driver
-RH_RF95 rf95(RFM95_CS, RFM95_INT);
+//RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
 // The shield uses the I2C SCL and SDA pins. On classic Arduinos
 // this is Analog 4 and 5 so you can't use those for analogRead() anymore
 // However, you can connect other I2C sensors to the I2C bus and share
 // the I2C bus.
 Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
+
+// These #defines make it easy to set the backlight color
+#define REVEILLE 0x1
+#define VEILLE 0x2
 
 
 // ---------- Initialisation des variables ---------------------
@@ -33,23 +45,27 @@ int Calc;
 int hallsensor = 23;    //The pin location of the sensor
 byte sensorInterrupt = 5;  // 0 = digital pin 2
 byte sensorPin       = 18; 
+
 float calibrationFactor = 4.5;
 volatile byte pulseCount;  
+
+
+// The hall-effect flow sensor outputs approximately 4.5 pulses per second per
+// litre/minute of flow.
 float flowRate;
 unsigned int flowMilliLitres;
 unsigned long totalMilliLitres;
-unsigned long oldTime;
 
+unsigned long oldTime;
 
 // Variables propres au DS18B20 (Sonde)
 const int DS18B20_PIN=22;
 const int DS18B20_ID=0x28;
-
 // Déclaration de l'objet ds
 OneWire ds(DS18B20_PIN); // on pin DS18B20_PIN
 
 // Variables générales
-float DS18B20_temperature=24;
+float DS18B20_temperature=24.00;
 const int SERIAL_PORT=9600;
 int time,compteur;
 int menu=0;
@@ -58,33 +74,54 @@ int idx=1;
 int fenetre;
 int t=0;
 
+//Elec
+int valeur;
+//Electrovanne
+int pin=45;
+int pin2=44;
 //globales variables addresse
-  byte buf[6]; //tab
+  byte buf[6]; //tab envoi
+  byte buf2[4]; //tab reception
   float niveau=0.0;
   float temp=0.0;
   unsigned int f=1;
   unsigned int a;
-  unsigned int crc=0;
+  unsigned int crc=255;
   unsigned int ok=0;
+  unsigned int verif;
+  unsigned int sw=0;
   int b;
-  unsigned int x=2;
+  unsigned int x=2; // position sur le LCD (x,y)
   unsigned int y=0;
   
   int addr = 0; // save données dans la ROM
   
-//PROTOCOLE PERSO LORA
+//PROTOCOLE PERSO LORA ENVOI
 #define ADRR 0
 #define FCT 1
 #define TEMP 2//3  //  b1 et b2 // b1 = partie entière, b2 =partie décimale
 #define NIVEAU 4//5  // b2 et b3 idem
 #define CRC 6 
+//PROTOCOLE PERSO LORA RECEPTION
+#define ADRR2 0
+#define FCT2 1
+#define DATA2 2//3
+#define CRC2 4 
 
 const int csPin = 40;          // LoRa radio chip select
 const int resetPin = 41;       // LoRa radio reset
 const int irqPin = 20; 
- int counter=0;
-void setup() {
 
+int counter=0; //compteur pour delay
+ 
+void setup() {
+  //electrovanne 
+  pinMode(23, OUTPUT);
+  pinMode(pin, OUTPUT);
+  digitalWrite(pin, HIGH);
+  pinMode(pin2, OUTPUT);
+  digitalWrite(pin2, HIGH);
+  
   Serial.begin(SERIAL_PORT);
   pinMode(sensorPin, INPUT);
   digitalWrite(sensorPin, HIGH);
@@ -93,16 +130,14 @@ void setup() {
   flowMilliLitres   = 0;
   totalMilliLitres  = 0;
   oldTime           = 0;
+  verif=0;
   attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
-
   Serial.println("Initialisation du programme");
-  
   // set up the LCD's number of columns and rows: 
   lcd.begin(16, 2);
 
   // Print a message to the LCD. We track how long it takes since
   // this library has been optimized a bit and we're proud of it :)
-  
   time = millis();
   time = millis() - time;
   Serial.print("Took "); Serial.print(time); Serial.println(" ms");
@@ -142,21 +177,6 @@ void loop() {   // DEBUT BOUCLE
     totalMilliLitres += flowMilliLitres;
       
     unsigned int frac;
-    
-    // Print the flow rate for this second in litres / minute
-    Serial.print("Flow rate: ");
-    Serial.print(int(flowRate));  // Print the integer part of the variable
-    Serial.print("L/min");
-    Serial.print("\t");       // Print tab space
-
-    // Print the cumulative total of litres flowed since starting
-    Serial.print("Output Liquid Quantity: ");        
-    Serial.print(totalMilliLitres);
-    Serial.println("mL"); 
-    Serial.print("\t");       // Print tab space
-  Serial.print(totalMilliLitres/1000);
-  Serial.print("L");
-    
 
     // Reset the pulse counter so we can start incrementing again
     pulseCount = 0;
@@ -164,11 +184,18 @@ void loop() {   // DEBUT BOUCLE
     // Enable the interrupt again now that we've finished sending output
     attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
   }
+  
+ // debit();
+  // print the number of seconds since reset:
 
     compteur=(millis()/1000);
 while (compteur/60==idx){
     DS18B20_temperature = getTemperatureDS18b20();  // Actualisation toutes les 60s de l'acquisition de la temp
     envoitrame();
+    delay(10);
+    ecoute();
+    delay(10);
+    valeur=analogRead(A15);
     idx++;
   break;
 }
@@ -193,6 +220,7 @@ case 1:
   lcd.setCursor(0, 1);
   lcd.print("Ox");
   lcd.print(a);
+  batterie();
 
   break;
  //-------------MENU 2 TEMP + DEBIT  
@@ -237,10 +265,11 @@ case 4:
 switch(bouton){
   case 1: // SELECT
       menu=4;
-
+      
     break;
     
   case 2: // RIGHT
+    envoitrame();
     menu++;
        if (menu>2){
         menu=1;
@@ -248,22 +277,29 @@ switch(bouton){
     lcd.clear();
     break;
   case 4: // DOWN
-    //lcd.setCursor(0, 1);
-   // lcd.print("Down ");
+       if (menu==2){
+    digitalWrite(pin, HIGH);  
+    digitalWrite(pin2, LOW);
+    delay(1000);
+    digitalWrite(pin2, HIGH);
+  }
      if (menu==3&&x==2&&ok==0){
     a--;
   }
     break;
   case 8:// UP
-  //  lcd.setCursor(0, 1);
-  //  lcd.print("UP   ");
+         if (menu==2){
+    digitalWrite(pin2, HIGH);
+    digitalWrite(pin, LOW);
+    delay(1000);
+    digitalWrite(pin, HIGH);
+  }
   if (menu==3&&x==2&&ok==0){
     a++;
   }
     break;
   case 16: // LEFT
-
-
+  
     menu--;
     if (menu<1){
         menu=2;
@@ -363,6 +399,7 @@ void initialisation(){
   lcd.clear();
   menu=1;
 }
+
 void rom(){
   
   b=byte(a);
@@ -389,11 +426,13 @@ void envoitrame(){
     buf[FCT]=byte(f);
     buf[ADRR]=byte(a);
     buf[CRC]=byte(crc);
+
+    
   while (!Serial);
 
   Serial.println("LoRa Sender");
 LoRa.setPins(csPin, resetPin, irqPin);
-  if (!LoRa.begin(915E6)) {
+  if (!LoRa.begin(RF95_FREQ)) {
     Serial.println("Starting LoRa failed!");
     while (1);
   }
@@ -422,13 +461,79 @@ LoRa.setPins(csPin, resetPin, irqPin);
 
   // send packet
   LoRa.beginPacket();
+  digitalWrite(23, HIGH);
+  digitalWrite(23, LOW);
   LoRa.write(buf,7);
+  Serial.print("Partie Entiere Temp : ");
+  Serial.println(buf[2]);
+  Serial.print("Partie Decimal Temp : ");
+  Serial.println(buf[3]);
+  digitalWrite(23, HIGH);
+  digitalWrite(23, LOW);
   LoRa.endPacket();
+  delay(60);
+  ecoute();
 }
 
 void pulseCounter()
 {
   // Increment the pulse counter
   pulseCount++;
+}
+
+void ecoute()
+{
+  if (!LoRa.begin(RF95_FREQ)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+ int packetSize = LoRa.parsePacket();
+   if (packetSize) 
+   {
+     // received a packet
+     int i= 0;
+     
+     Serial.print("receive packet of ");
+     Serial.print(packetSize);
+     Serial.print(" bytes : ");
+     
+     // read packet
+     while (LoRa.available()) {
+       byte b = LoRa.read();
+       buf2[i]=b;
+       
+       Serial.print(b);
+       Serial.print(":");
+       i++;
+     }
+
+     int crc = 0;
+     for (int x=0; x < packetSize-1;x++)
+     {
+       for (int j=0; j<8; j++)
+       {
+         crc += buf2[x]>>j & 0x01;
+       }
+     }
+
+     //code de fonction
+     if(buf2[FCT]==1)//code fonction (all) & recv[packetSize-1] == crc
+     {
+      sw=1;
+     }
+     else if(buf2[FCT]==2){
+      sw=2;
+     }
+   }
+  
+}
+void batterie(){
+  //Serial.print(analogRead(A15));
+  valeur=analogRead(A15);
+  float tension=valeur*((5.0/1023.0)*3.24); //coef pont diviseur
+  lcd.print("   ");
+  lcd.print("Bat:");
+  lcd.print(tension);
+  lcd.print("V");
 }
 
